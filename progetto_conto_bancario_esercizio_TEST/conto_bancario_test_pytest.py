26944ef749dcc17ab98004ef5a2db51e5e1e0711
DennisXIII <dennisesposito.business@gmail.com>
2025-06-04 09:43:21 +0200

    caricati file progetto

---
+++ progetto_conto_bancario_esercizio_TEST/conto_bancario_test_pytest.py
@@ -0,0 +1,29 @@
+import pytest
+
+from progetto_conto_bancario_esercizio_TEST.conto_bancario import ContoBancario
+
+
+@pytest.fixture   # è equivalente al def setup di unittest, è un decoratore che contiene il costruttore della classe test, o comunque altre funzioni
+def conto():
+    return ContoBancario("Mario Rossi", 100)
+
+
+def test_creazione_conto(conto):
+    # self.assertEqual(self.conto.nome, "Mario Rossi")
+    # self.assertEqual(self.conto.saldo, 100)
+    assert conto.nome == "Mario Rossi"
+    assert conto.saldo == 100
+
+def test_deposita_conto(conto):
+    # self.conto.deposita(50)
+    # self.assertEqual(self.conto.saldo, 150)
+    conto.deposita(50)
+    assert conto.saldo == 150
+
+def test_deposito_negativo(conto):
+    # with self.assertRaises(ValueError):
+    #     self.conto.deposita(-20)
+    # self.assertEqual(self.conto.saldo, 100)
+    with pytest.raises(ValueError):
+         conto.deposita(-20)
+    assert conto.saldo == 100
